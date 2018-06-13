989a486fafb2c11cd04801c016c2cbf0814fb2bc
Vania THAI <vania@enclave.vn>
2018-06-13 14:10:18 +0700

    Initial Commit

---
+++ DiceRoll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiceRoll
+//
+//  Created by Van (Vania) T.H. THAI on 6/13/18.
+//  Copyright © 2018 Van (Vania) T.H. THAI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
